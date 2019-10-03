a7e1632621b448eb3d54efa4367cde658ddbbbb4
adikeshri <keshri.aditya999@gmail.com>
2019-10-04 00:28:40 +0530

    Timer end time updated to 9:30.

--- vendors/flipclock/timer.js
+++ vendors/flipclock/timer.js
@@ -1,7 +1,7 @@
 var today = new Date();
 
 var timer = function() {};
-timer.countdownDate = new Date('October 18, 2019 00:23:00');
+timer.countdownDate = new Date('October 18, 2019 09:30:00');
 
 // set date to 10 days in the future for testing purposes
 timer.countdownDate.setDate( timer.countdownDate.getDate() );
